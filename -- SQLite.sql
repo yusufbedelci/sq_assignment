5ef67e73bd04721e93c2bd04867cda746cd5ac12
Arief Badal <ariefbadal21@outlook.com>
2024-06-01 16:25:26 +0200

    Update SQL script

--- -- SQLite.sql
+++ -- SQLite.sql
@@ -1,18 +1,4 @@
 -- SQLite
-PRAGMA foreign_keys = ON;
-INSERT INTO user (assigned_role_id, name, lastname, age, gender, weight, email, mobile, password)
-VALUES (1,'Arief', '', 20, 'male',200,'test2@test.nl',0612341566,'test1234');
-
-
-PRAGMA foreign_keys = ON;
-INSERT INTO role (role_id,name)
-VALUES (1,'admin');
-
-PRAGMA foreign_keys = ON;
-INSERT INTO user_role(user_id, role_id) VALUES (1,1);
-
-INSERT INTO address (streetname, house_number, zipcode, city)
-VALUES ('Bakerstreet', 102, 4010, 'New York');
-
-INSERT INTO membership (user_id, registration_date)
-VALUES (1,'1-6-2024');
+SELECT name || ' ' || lastname AS fullname, user_id AS  id, email,password, mobile, age, gender, weight 
+FROM user INNER JOIN user_profile ON user.user_id = user_profile.profile_id
+WHERE email='super@company.nl' AND password='test1234';
