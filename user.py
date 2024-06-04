fa18bc17045998999709e70e3ee9cc8b7d13d7f0
Arief Badal <ariefbadal21@outlook.com>
2024-06-04 15:30:46 +0200

    Add other attributes for an user

--- user.py
+++ user.py
@@ -1,10 +1,14 @@
 class User:
-    def __init__(self, id,role_id,name, email, password, mobile):
+    def __init__(self, id,role_id,username, email, password, mobile, fullname, age, gender, weight):
         self.id = id
-        self.name=name
+        self.fullname = fullname
+        self.role_id= role_id
         self.email=email
+        self.username = username
         self.password=password
         self.mobile=mobile
-        self.role_id= role_id
+        self.age = age
+        self.gender = gender
+        self.weight =weight
     
     
