6e8135d986dc8a5aebb68920af375854952d1848
Arief Badal <ariefbadal21@outlook.com>
2024-06-07 22:25:45 +0200

    Add seperated out of SQL logic

---
+++ database/membership.py
@@ -0,0 +1,15 @@
+CREATE_MEMBERSHIP_TABLE = """
+				CREATE TABLE IF NOT EXISTS membership (
+				id INTEGER PRIMARY KEY,
+				member_user_id INTEGER,
+				registration_date TEXT NOT NULL,
+				FOREIGN KEY(member_user_id) REFERENCES user(user_id)
+
+				);
+			"""
+
+
+def initialize_membership_table(con):
+    cursor = con.cursor()
+    cursor.execute(CREATE_MEMBERSHIP_TABLE)
+    con.commit()
