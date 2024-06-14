30a7c80b3cb89f5f9d859df8a39e63c4863d1748
Arief Badal <1042170@hr.nl>
2024-06-14 17:00:32 +0200

    remove database

--- -- SQLite.sql
+++
@@ -1,4 +0,0 @@
--- SQLite
-SELECT name || ' ' || lastname AS fullname, user_id AS  id, email,password, mobile, age, gender, weight 
-FROM user INNER JOIN user_profile ON user.user_id = user_profile.profile_id
-WHERE email='super@company.nl' AND password='test1234';
