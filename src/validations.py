2cf9947fd95d7998633712a2d1ebd43caeb56dcd
Arief Badal <ariefbadal21@outlook.com>
2024-09-16 12:00:51 +0200

    Refactor of validation functions

--- src/validations.py
+++ src/validations.py
@@ -3,49 +3,79 @@ import re
 
 def validate_username(username):
     pattern = r"^(?!.*[_.]{2})[a-zA-Z_](?:[\w.\'-]{6,8})$"
-    return bool(re.match(pattern, username))
+    if re.match(pattern, username):
+        return True
+    return False
 
 
 def validate_password(password):
     pattern = r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[~!@#$%&_\-+=`|\\(){}[\]:;\'<>,.?/])[A-Za-z\d~!@#$%&_\-+=`|\\(){}[\]:;\'<>,.?/]{12,30}$"
-    return bool(re.match(pattern, password))
+    if re.match(pattern, password):
+        return True
+    return False
 
 
 def validate_name(name):
     pattern = r"^[a-zA-Z]{2,20}$"
-    return bool(re.match(pattern, name))
+    if re.match(pattern, name):
+        return True
+    return False
 
 
 def validate_age(age):
     pattern = r"^(?:[1-9][0-9]?|100)$"
-    return bool(re.match(pattern, age))
+    if re.match(pattern, age):
+        return True
+    return False
 
 
 def validate_weight(weight):
     pattern = r"^(?:[1-9][0-9]?|1[0-9]{2}|200|201)$"  # 1-201 KG
-    return bool(re.match(pattern, str(weight)))
+    if re.match(pattern, str(weight)):
+        return True
+    return False
 
 
 def validate_email(email):
-    pattern = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
-    return bool(re.match(pattern, email))
+    """ 
+    Username part (before the @): between 1 and 64 characters.
+    The domain part (after the @) to a range of between 1 and 255.
+    The TLD is between 2 and 24 characters.
+    """
+    pattern = r"^[a-zA-Z0-9_.+-]{1,64}@[a-zA-Z0-9-]{1,255}\.[a-zA-Z]{2,24}$"
+
+    if re.match(pattern, email):
+        return True
+    return False
 
 
 def validate_phone_number(phone_number):
     pattern = r"^(?:\+?31|0)(?:[1-9][0-9]?|6[1-6])\d{7}$"  # Dutch phone number
-    return bool(re.match(pattern, phone_number))
+    if re.match(pattern, phone_number):
+        return True
+    return False
 
 
 def validate_street_name(street_name):
     pattern = r"^[a-zA-Z0-9\s]{2,30}$"
-    return bool(re.match(pattern, street_name))
+    if re.match(pattern, street_name):
+        return True
+    return False
 
 
 def validate_house_number(house_number):
     pattern = r"^[1-9][0-9]{0,3}[a-zA-Z]{0,1}$"
-    return bool(re.match(pattern, house_number))
-
-
+    if re.match(pattern, house_number):
+        return True
+    return False
+    
 def validate_zip_code(zip_code):
     pattern = r"^[1-9][0-9]{3}\s?[a-zA-Z]{2}$"
-    return bool(re.match(pattern, zip_code))
+    if re.match(pattern, zip_code):
+        return True
+    return False
+
+def validate_server_input(option, options):
+    if option in options:
+        return True
+    return False
