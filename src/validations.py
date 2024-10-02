2212b2d3667acddb3501c9dfbe4fa13c5723a4cf
Yusuf <ybedelci@gmail.com>
2024-10-02 16:31:30 +0200

    renamed valid method to is_valid

--- src/validations.py
+++ src/validations.py
@@ -1,41 +1,41 @@
 import re
 
 
-def valid(value, pattern):
+def is_valid(value, pattern):
     return re.match(pattern, value) and "\x00" not in value
 
 
 def validate_username(username):
     pattern = r"^(?!.*[_.]{2})[a-zA-Z_](?:[\w.\'-]{6,8})$"
-    if valid(username, pattern):
+    if is_valid(username, pattern):
         return True
     return False
 
 
 def validate_password(password):
     pattern = r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[~!@#$%&_\-+=`|\\(){}[\]:;\'<>,.?/])[A-Za-z\d~!@#$%&_\-+=`|\\(){}[\]:;\'<>,.?/]{12,30}$"
-    if valid(password, pattern):
+    if is_valid(password, pattern):
         return True
     return False
 
 
 def validate_name(name):
     pattern = r"^[a-zA-Z]{2,20}$"
-    if valid(name, pattern):
+    if is_valid(name, pattern):
         return True
     return False
 
 
 def validate_age(age):
     pattern = r"^(?:[1-9][0-9]?|100)$"
-    if valid(age, pattern):
+    if is_valid(age, pattern):
         return True
     return False
 
 
 def validate_weight(weight):
     pattern = r"^(?:[1-9][0-9]?|1[0-9]{2}|200|201)$"  # 1-201 KG
-    if valid(weight, pattern):
+    if is_valid(weight, pattern):
         return True
     return False
 
@@ -47,35 +47,35 @@ def validate_email(email):
     The TLD is between 2 and 24 characters.
     """
     pattern = r"^[a-zA-Z0-9_.+-]{1,64}@[a-zA-Z0-9-]{1,255}\.[a-zA-Z]{2,24}$"
-    if valid(email, pattern):
+    if is_valid(email, pattern):
         return True
     return False
 
 
 def validate_phone_number(phone_number):
     pattern = r"^(?:\+?31|0)(?:[1-9][0-9]?|6[1-6])\d{7}$"  # Dutch phone number
-    if valid(phone_number, pattern):
+    if is_valid(phone_number, pattern):
         return True
     return False
 
 
 def validate_street_name(street_name):
     pattern = r"^[a-zA-Z0-9\s]{2,30}$"
-    if valid(street_name, pattern):
+    if is_valid(street_name, pattern):
         return True
     return False
 
 
 def validate_house_number(house_number):
     pattern = r"^[1-9][0-9]{0,3}[a-zA-Z]{0,1}$"
-    if valid(house_number, pattern):
+    if is_valid(house_number, pattern):
         return True
     return False
 
 
 def validate_zip_code(zip_code):
     pattern = r"^[1-9][0-9]{3}\s?[a-zA-Z]{2}$"
-    if valid(zip_code, pattern):
+    if is_valid(zip_code, pattern):
         return True
     return False
 
